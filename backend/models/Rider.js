e88b18f6143c73d47f980f927d16b9df7d550fba
Iresh Eranga <iresheranga2020al@gmail.com>
2024-03-20 12:48:33 +0530

    rider modal

---
+++ backend/models/Rider.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+const USER_ROLES = require("../constants/roles");
+
+const riderSchema = new mongoose.Schema(
+    {
+        employeeid:{type:String,required:true},
+        name:{type:String, required:true},
+        email:{type:String, required:true, unique:true},
+        password:{type:String, required:true},
+        role:{
+            type:String,
+            enum:Object.values(USER_ROLES),
+            default:USER_ROLES.RIDER,
+        },
+        image: { type: String },
+        address: { type: String },
+        contact: { type: Number },
+        //rides: { type: Schema.Types.ObjectId, ref: "Order" },
+
+    },
+    {
+        timestamps:true,
+    }
+);
+
+module.exports = mongoose.model("Rider", riderSchema);
