e1edd391b5e6383528fe14ba56369bfacb07b60e
Iresh Eranga <iresheranga2020al@gmail.com>
2024-03-20 12:53:31 +0530

    auth controller for ridersignup - not working
    not working

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
 const User = require("../models/User");
+const Rider = require("../models/Rider");
 const USER_ROLES = require("../constants/roles");
 const welcomeEmailTemplate = require("../util/email_templates/welcomeEmailTemplate");
 const sendEmail = require("../util/sendEmail");
@@ -99,6 +100,67 @@ const authController = {
           role: newSupplier.role,
         },
         message: "Supplier created successfully",
+      });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({
+        success: false,
+        error,
+        message: "Internal server error",
+      });
+    }
+  },
+
+
+  riderSignup: async (req, res) => {
+    try {
+      const { employeeid, name,email, address, contact,image } = req.body;
+      const role = USER_ROLES.RIDER;
+
+      const existingRider = await Rider.findOne({ email });
+      if (existingRider) {
+        return res.status(409).json({
+          success: false,
+          message: "Rider already exists",
+        });
+      }
+
+      // auto-generate password with 8 characters
+      const password = Math.random().toString(36).slice(-8);
+      const hashedPassword = await bcrypt.hash(password, 10);
+
+      const newRider = new Rider({
+        employeeid,
+        name,
+        email,
+        password: hashedPassword,
+        role,
+        address,
+        contact,
+        image,
+      });
+      await newRider.save();
+
+      // Send welcome email to the supplier
+      const emailTemplate = welcomeEmailTemplate(name, email, password);
+      //
+      sendEmail(email, "Welcome to Emerald Bay!", emailTemplate);
+
+      res.status(201).json({
+        success: true,
+        rider: {
+          _id: newRider._id,
+          employeeid: newRider.employeeid,
+          name: newRider.name,
+          email: newRider.email,
+          password: newRider.password,
+          role: newRider.role,
+          address:newRider.address,
+          contact:newRider.contact,
+          image:newRider.image,
+          
+        },
+        message: "Rider created successfully",
       });
     } catch (error) {
       console.error(error);
