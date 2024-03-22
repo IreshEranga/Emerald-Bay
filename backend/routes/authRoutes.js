87e32760ec1f5f2aaf9c99f8935186888797290e
Iresh Eranga <iresheranga2020al@gmail.com>
2024-03-22 17:43:58 +0530

    backend setup for delivery rider
    CRUD complete

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -5,6 +5,7 @@ const authMiddleware = require("../middleware/authMiddleware");
 const router = express.Router();
 
 router.post("/supplier/signup", authMiddleware(["ADMIN"]), authController.supplierSignup);
+router.post("/rider/signup", authMiddleware(["ADMIN"]), authController.riderSignup);
 router.post("/login", authController.login);
 
 module.exports = router;
