ac3daed74ade81bfb5c605cc7cd5249a75ad8d06
Kavini Wickramasooriya <165365634+KaviniWickramasooriya@users.noreply.github.com>
2024-04-08 21:42:08 +0530

    update nav bars

--- frontend/src/components/Navbar_customer.js
+++ frontend/src/components/Navbar_customer.js
@@ -2,7 +2,6 @@ import React from 'react';
 import Navbar from 'react-bootstrap/Navbar';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
-import Button from 'react-bootstrap/Button';
 import logo from '../assets/EMERALDBAYLOGO.png';
 import { LinkContainer } from 'react-router-bootstrap';
 import { FaShoppingCart, FaUser } from 'react-icons/fa'; // Importing icons
@@ -10,16 +9,16 @@ import { FaShoppingCart, FaUser } from 'react-icons/fa'; // Importing icons
 
 function Navbar_customer({ isAuthenticated, user, logout }) {
   return (
-    <Navbar bg="dark" expand="lg" className="bg-body-tertiary" style={{ fontSize: '25px', height: '170px' }}>
+    <Navbar bg="dark" expand="lg" className="bg-body-tertiary" style={{ fontSize: '25px', height: '150px' }}>
       <Container fluid>
         <Navbar.Brand href="#">
-          <img src={logo} alt="logo" width={'180px'} height={'150px'} />
+          <img src={logo} alt="logo" width={'150px'} height={'120px'} />
         </Navbar.Brand>
 
         <Navbar.Toggle aria-controls="navbarScroll" />
-        <Navbar.Collapse id="navbarScroll" style={{ marginLeft: '250px' }}>
+        <Navbar.Collapse id="navbarScroll">
 
-          <Nav className="me-auto my-2 my-lg-0 ms-auto" style={{ maxHeight: '100px', gap: '40px', textAlign: 'left' }} navbarScroll>
+          <Nav className="me-auto my-2 my-lg-0 ms-auto" style={{ maxHeight: '100px', gap: '60px', textAlign: 'left'}} navbarScroll>
             <LinkContainer to="/customer">
               <Nav.Link>Home</Nav.Link>
             </LinkContainer>
@@ -27,15 +26,13 @@ function Navbar_customer({ isAuthenticated, user, logout }) {
               <Nav.Link>Menu</Nav.Link>
             </LinkContainer>
             <LinkContainer to="/customer_reservations">
-              <Nav.Link>Reservations</Nav.Link>
+              <Nav.Link style={{ marginRight: '100px' }}>Reservations</Nav.Link>
             </LinkContainer>
-            {/* Cart Icon */}
             <LinkContainer to="/view_cart">
-              <Nav.Link>
+              <Nav.Link style={{ marginLeft: '200px' }}>
                 <FaShoppingCart size={24} />
               </Nav.Link>
             </LinkContainer>
-            {/* Profile Icon */}
             <LinkContainer to="/view_profile">
               <Nav.Link>
                 <FaUser size={24} />
