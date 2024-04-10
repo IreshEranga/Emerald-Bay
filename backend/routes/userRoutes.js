a422fbb1cce22e432ac3e925bf057e3c2b01cf83
Iresh Eranga <iresheranga2020al@gmail.com>
2024-04-10 12:53:21 +0530

    Update userRoutes.js

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -4,6 +4,6 @@ const authMiddleware = require("../middleware/authMiddleware");
 
 const router = express.Router();
 
-router.get("/", authMiddleware(["ADMIN"]), userController.getAllUsers);
+router.get("/", authMiddleware(["ADMIN","DELIVERYMANAGER"]), userController.getAllUsers);
 
 module.exports = router;
