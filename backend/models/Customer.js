bf210435eed2579264876e8aeb68be110ac9f999
Iresh Eranga <iresheranga2020al@gmail.com>
2024-04-12 01:00:29 +0530

    Update Customer.js
    increment cus id

--- backend/models/Customer.js
+++ backend/models/Customer.js
@@ -1,12 +1,13 @@
 const mongoose = require("mongoose");
 const USER_ROLES = require("../constants/roles");
 
-
-// Create student schema
-
+// Create customer schema
 const customerSchema = new mongoose.Schema({
-
-    
+    customerId: {
+        type: String,
+        
+        unique: true
+    },
     name: {
         type: String,
         required: true
@@ -19,21 +20,34 @@ const customerSchema = new mongoose.Schema({
         type: Number,
         required: true
     },
-    password:{
-        type:String,
+    password: {
+        type: String,
     },
-    status:{
-        type:String,
-        default:"Normal",
+    status: {
+        type: String,
+        default: "Normal",
     },
-    address:{
-        type:String,
+    address: {
+        type: String,
     },
-    role:{
-        type:String,
-        default:USER_ROLES.CUSTOMER,
+    role: {
+        type: String,
+        default: USER_ROLES.CUSTOMER,
     },
-})
+});
 
+// Define a function to generate the next customer ID
+async function generateNextCustomerId() {
+    const lastCustomer = await this.findOne({}, {}, { sort: { 'customerId': -1 } });
+    const lastCustomerId = lastCustomer ? parseInt(lastCustomer.customerId, 10) : 0;
+    return (lastCustomerId + 1).toString();
+}
 
-module.exports = mongoose.model("Customer", customerSchema);;
+customerSchema.pre("save", async function (next) {
+    if (!this.customerId) {
+        this.customerId = await generateNextCustomerId.call(this.constructor);
+    }
+    next();
+});
+
+module.exports = mongoose.model("Customer", customerSchema);
