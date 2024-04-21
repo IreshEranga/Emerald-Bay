e73c4ce1a3ec8e2fd0f607d460ce0b982f9001e7
Iresh Eranga <iresheranga2020al@gmail.com>
2024-04-21 10:14:11 +0530

    customer id auto increment complete

--- backend/models/Customer.js
+++ backend/models/Customer.js
@@ -37,16 +37,17 @@ const customerSchema = new mongoose.Schema({
 });
 
 // Define a function to generate the next customer ID
-async function generateNextCustomerId() {
-    const lastCustomer = await this.findOne({}, {}, { sort: { 'customerId': -1 } });
-    const lastCustomerId = lastCustomer ? parseInt(lastCustomer.customerId, 10) : 0;
-    return (lastCustomerId + 1).toString();
-}
-
 customerSchema.pre("save", async function (next) {
-    if (!this.customerId) {
-        this.customerId = await generateNextCustomerId.call(this.constructor);
+    const Customer = this.constructor;
+    const lastCustomer = await Customer.findOne({}, {}, { sort: { customerId: -1 } });
+    let newCustomerId;
+    if (lastCustomer) {
+        const lastCustomerId = parseInt(lastCustomer.customerId.slice(-2)); // Extract the numerical part from the last employeeid
+        newCustomerId = `cus${(lastCustomerId + 1).toString().padStart(2, "0")}`; // Increment and format the new employeeid
+    } else {
+        newCustomerId = "cus01"; // If there are no riders yet, start from 01
     }
+    this.customerId = newCustomerId;
     next();
 });
 
