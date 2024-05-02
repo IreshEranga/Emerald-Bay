2c42b4c1c7237d483707a4db943ae292f1c748ac
Iresh Eranga <iresheranga2020al@gmail.com>
2024-05-02 19:10:45 +0530

    Update useRiderData.js
    remove unnecessary comment

--- frontend/src/hooks/useRiderData.js
+++ frontend/src/hooks/useRiderData.js
@@ -8,10 +8,7 @@ export const useRiderData = () => {
 export const useRiderCount = () => {
   return useQuery(["riderCount"], () => RiderAPI.getCount());
 };
-/*
-export const useAvailableStock = () => {
-  return useQuery(["availableStock"], () => RiderAPI.getAvailableStock());
-};*/
+
 
 export const useRider = (id) => {
   return useQuery(["rider", id], () => RiderAPI.getById(id));
