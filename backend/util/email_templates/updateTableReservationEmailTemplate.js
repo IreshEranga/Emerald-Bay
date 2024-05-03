882f7e6f2a658a4cb47faebdea15a1f6838d7b60
Kavini Wickramasooriya <165365634+KaviniWickramasooriya@users.noreply.github.com>
2024-05-03 11:13:44 +0530

    reservations update

--- backend/util/email_templates/updateTableReservationEmailTemplate.js
+++ backend/util/email_templates/updateTableReservationEmailTemplate.js
@@ -1,12 +1,13 @@
-const updateTableReservationEmailTemplate = (reservationId, date, time, tableNo) => {
+const updateTableReservationEmailTemplate = (name, reservationId, date, time, tableNo) => {
     return `
+        <p>Dear ${name},</p>
         <p>Your table reservation with ID ${reservationId} has been successfully updated.</p>
         <p>Here are your new Reservation Details : </p>
         <p> • Date: ${date}</p>
         <p> • Time: ${time}</p>
         <p> • Table Number: ${tableNo}</p>
         <p>Thank you for choosing our restaurant. We look forward to creating an unforgettable reservation experience for you.</p>
-        <p>Need to make changes to this reservation ? If so, call 0772120231</p>
+        <p>Need to make changes to this reservation ? If so, call 0772120231.</p>
         <p>Feel free to sprinkle a touch of magic on your reservation by reaching out to our friendly team!</p>
         <p><br/></p>
         <p>Best regards,</p>
